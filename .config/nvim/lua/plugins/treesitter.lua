62083b87be71809fe480c20ea827d0dab317e009
Erick Contreras <er.contreras@hotmail.com>
2025-04-16 15:18:58 -0600

    Remove unnecessary code

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,23 +5,6 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = {
-          "toml",
-          "c",
-          "lua",
-          "javascript",
-          "ruby",
-          "css",
-          "html",
-          "embedded_template",
-          "json",
-          "yaml",
-          "python",
-          "bash",
-          "zsh",
-          "rust",
-          "wgsl",
-        },
         auto_install = true,
         highlight = { enable = true },
         --indent = { enable = true },
