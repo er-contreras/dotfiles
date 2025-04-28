32a46bb13b43a9ac6fe0cb95e67f673444cd6d1f
Erick Contreras <er.contreras@hotmail.com>
2025-04-27 21:37:41 -0600

    lazy commit

--- .config/slstatus/config.def.h
+++ .config/slstatus/config.def.h
@@ -65,7 +65,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%T %A %e" },
+	{ datetime, "%s ",           "%T %A %e" },
+	{ battery_perc, "| Battery: %s%%", 		"BAT0" },
   /* { cpu_perc, " | CPU: %s%%",         NULL }, */
   /* { ram_perc, " | RAM: %s%%",         NULL }, */
 };
--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# xrandr --output LVDS-1 --off eDP-1
+# xrandr --output HDMI-1 --primary --auto --output LVDS-1 --off
 exec slstatus &
 nitrogen --restore &
 exec compton &
