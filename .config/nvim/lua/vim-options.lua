cc226a98bfe15eed45fc9da8ade0ca24ecaaa7af
ercntreras <er.contreras@hotmail.com>
2026-02-20 22:03:08 -0600

    Update Neovim syntax colors to classic vim colors

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,5 +1,5 @@
 vim.opt.clipboard:append("unnamedplus")
--- vim.cmd("syntax on")
+-- vim.cmd("syntax off")
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
@@ -9,36 +9,23 @@ vim.g.mapleader = " "
 vim.cmd("inoremap kj <ESC>")
 vim.cmd("nnoremap ; <C-w>")
 vim.diagnostic.config({ virtual_text = true })
--- vim.cmd("inoremap <c-b> <Esc>:Lex<cr>:vertical resize 30<cr>")
--- vim.cmd("nnoremap <c-b> <Esc>:Lex<cr>:vertical resize 30<cr>")
 vim.cmd("set encoding=UTF-8")
 
 -- Revert to classic Vim color palette
-vim.cmd("colorscheme retrobox")
+vim.cmd("colorscheme vim")
+vim.o.background = "light"
+vim.opt.termguicolors = false
 
 -- Customize specific colors
 local hl = vim.api.nvim_set_hl
--- Change Function colors
-hl(0, "Function", { fg = "#ebdbb2", bold = true })
--- Change Directory colors
-hl(0, "Directory", { fg = "#458588", bold = true })
 
--- vim.o.background = "dark"
--- Disable TrueColor if you want the terminal to handle the 'dimming'
--- This often makes the colors look exactly like the first photo
--- vim.opt.termguicolors = false
+hl(0, "Identifier", { fg = "#ffffff", bold = false })
+hl(0, "Operator", { fg = "#ffffff", bold = false })
+hl(0, "Delimiter", { fg = "#ffffff", bold = false })
+hl(0, "Special", { ctermfg = 2, bold = false })
+hl(0, "LineNr", { ctermfg = 8, bold = false })
+hl(0, "Character", { ctermfg = 5, bold = false })
+hl(0, "SpecialChar", { ctermfg = 5, bold = false })
 
 -- Transparency Settings
--- local highlight_groups = {
---   "Normal",
---   "NormalNC",
---   "LineNr",
---   "SignColumn",
---   "StatusLine",
---   "StatusLineNC",
---   "EndOfBuffer",
--- }
--- 
--- for _, group in ipairs(highlight_groups) do
---   vim.api.nvim_set_hl(0, group, { bg = "none" })
--- end
+hl(0, "SignColumn", { bg = "none" })
